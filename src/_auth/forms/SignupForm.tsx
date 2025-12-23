81d34c902c91fdfe372331ed368cbcdb3e58c1bf
Dharm Patel <pdharm774@gmail.com>
2025-12-23 23:23:13 +0530

    added signup logic

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -25,10 +25,8 @@ import { Link } from "react-router-dom";
     })
   
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof SignupValidation>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      console.log(values)
+    async function onSubmit(values: z.infer<typeof SignupValidation>) {
+      // const newUser = await createUserAccount(values)
     }
     return (
       <Form {...form}>
